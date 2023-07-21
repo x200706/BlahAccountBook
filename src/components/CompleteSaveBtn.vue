98913b37aef7206076a77ac2dd61b81fc477d580
x200706 <99391710+x200706@users.noreply.github.com>
2023-07-21 11:52:11 +0000

    add a component and an index page content

---
+++ src/components/CompleteSaveBtn.vue
@@ -0,0 +1,31 @@
+<!--讓他可以傳入成功或失敗 v-if判斷-->
+<template>
+  <button ref="liveToastBtn" type="button" class="btn btn-primary" @click="showToast">儲存</button>
+  <div class="position-fixed bottom-0 end-0 p-3" style="z-index: 11">
+    <div id="liveToast" class="toast" role="alert" aria-live="assertive" aria-atomic="true">
+      <div class="toast-header">
+        <strong class="me-auto">提示</strong>
+        <button type="button" class="btn-close" data-bs-dismiss="toast" aria-label="Close"></button>
+      </div>
+      <div class="toast-body">
+        儲存成功
+      </div>
+    </div>
+  </div>
+</template>
+
+<script setup>
+import { ref, onMounted } from "vue";
+
+const liveToastBtn = ref(null);
+
+const showToast = () => {
+  const toastLiveExample = document.getElementById('liveToast');
+  const toast = new bootstrap.Toast(toastLiveExample);
+  toast.show();
+}
+
+onMounted(() => {
+  console.log(liveToastBtn.value);
+});
+</script>
--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -1,28 +1,82 @@
+<!--之後還想做上鎖功能-->
 <script setup>
+  import CompleteSaveBtn from './CompleteSaveBtn.vue'
 </script>
 
 <template>
 <nav class="navbar navbar-dark bg-dark">
     <div class="container-fluid">
-      <a class="navbar-brand" href="#">Navbar</a>
+      <a class="navbar-brand" href="#">BlahAccountBook</a>
     </div>
 </nav>
 
 <div class="container">
   <div class="row">
     <div class="col">
-      Column
+<!--我是左邊-->
+      每月支出<br>
+      每月支出細節
+    </div>
+    <div class="col-6">
+      <!-- Button trigger modal -->
+      總資產
+      總收入
+<div class="account-btn"><button type="button" class="btn btn-primary" data-bs-toggle="modal" data-bs-target="#exampleModal" align="left">
+  記帳
+</button>
+  </div>
+
+<!-- Modal -->
+<div class="modal fade" id="exampleModal" tabindex="-1" aria-labelledby="exampleModalLabel" aria-hidden="true">
+  <div class="modal-dialog">
+    <div class="modal-content">
+      <div class="modal-header">
+        <h5 class="modal-title" id="exampleModalLabel">記帳</h5>
+        <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+      </div>
+      <div class="modal-body">
+        表單預留位
+      </div>
+      <div class="modal-footer">
+        <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">取消</button>
+        <CompleteSaveBtn/>
+      </div>
+    </div>
+  </div>
+</div>
+<table class="table">
+  <thead class=table-dark>
+    <tr>
+      <th scope="col">項目</th>
+      <th scope="col">日期</th>
+      <th scope="col">收/支</th>
+      <th scope="col">分類</th>
+      <th scope="col">金額</th>
+    </tr>
+  </thead>
+  <tbody>
+    <tr>
+      <td>洗碗精</td><!--希望飄上去有memo欸-->
+      <td>2023/07/19</td>
+      <td>支出</td>
+      <td>日用品</td>
+      <td>70</td>
+    </tr>
+  </tbody>
+</table>
     </div>
     <div class="col">
-      Column
-    </div>
-    <div class="col">
-      Column
+<!--我是右邊-->
+      總支出分布<br>
+      開銷Top10
     </div>
   </div>
 </div>
 </template>
-
 <style scoped>
-
+.account-btn{
+  text-align: right;
+  margin-bottom: 10px;
+  margin-top: 10px;
+}
 </style>
