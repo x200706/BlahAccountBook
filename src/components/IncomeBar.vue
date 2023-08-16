f4d236580f3b2cb2aedfe2f10c3d630dca9706c6
Crystal <chi200706@gmail.com>
2023-08-16 22:37:44 +0800

    chartjs test

--- src/components/IncomeBar.vue
+++ src/components/IncomeBar.vue
@@ -5,6 +5,7 @@
 </template>
 <script setup>
 import { onMounted } from 'vue';
+const testList = [39400, 57400, 39400, 47400, 39400, 42400, 39400]
 
 onMounted(() => {
   const ctx = document.getElementById('myChart3');
@@ -14,7 +15,7 @@ onMounted(() => {
       labels: [ '1', '2', '3', '4', '5', '6','7','8','9','10','11','12'],
       datasets: [{
         label: '收入',
-        data: [39400, 57400, 39400, 47400, 39400, 42400, 39400],
+        data: testList,
         borderWidth: 1,
         backgroundColor:[
         '#B0CCED',
