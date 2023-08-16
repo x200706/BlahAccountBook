0fc9fe8883c4289728e3949bad9096ab17039fe4
Crystal <chi200706@gmail.com>
2023-08-16 22:42:37 +0800

    chartjs code arrange

--- src/components/IncomeBar.vue
+++ src/components/IncomeBar.vue
@@ -1,21 +1,18 @@
-<template>
-  <div class="main-chart-container">
-    <canvas id="myChart3"></canvas>
-  </div>
-</template>
 <script setup>
 import { onMounted } from 'vue';
-const testList = [39400, 57400, 39400, 47400, 39400, 42400, 39400]
+
+const labelsList = [ '1', '2', '3', '4', '5', '6','7','8','9','10','11','12']
+const dataList = [39400, 57400, 39400, 47400, 39400, 42400, 39400]
 
 onMounted(() => {
   const ctx = document.getElementById('myChart3');
   new Chart(ctx, {
     type: 'bar',
     data: {
-      labels: [ '1', '2', '3', '4', '5', '6','7','8','9','10','11','12'],
+      labels: labelsList,
       datasets: [{
         label: '收入',
-        data: testList,
+        data: dataList,
         borderWidth: 1,
         backgroundColor:[
         '#B0CCED',
@@ -34,3 +31,8 @@ onMounted(() => {
   });
 });
 </script>
+<template>
+  <div class="main-chart-container">
+    <canvas id="myChart3"></canvas>
+  </div>
+</template>
